6d870cca2ea336b2205f18da88376d31077325b5
Sergey Pyshkin <tamplier_74@mail.ru>
2021-08-31 22:28:59 +0500

    Вторая итерация

--- scripts/Javascript.js
+++ scripts/Javascript.js
@@ -1,29 +1,42 @@
-let OpenPopupButton = document.querySelector('.profile__edit-button');
-let ClosePopupButton = document.querySelector('.popup__close');
+let openPopupButton = document.querySelector('.profile__edit-button');
+let closePopupButton = document.querySelector('.popup__close');
 let popup = document.querySelector('.popup');
 let nameProfile = document.querySelector('.profile__title');
 let jobProfile = document.querySelector('.profile__subtitle');
 let namePopup = document.querySelectorAll('.popup__input')[0];
 let jobPopup = document.querySelectorAll('.popup__input')[1];
 let form = document.querySelector('.popup__form');
-let submitButton = document.querySelector('popup__submit');
 
-function togglePopup() {
-  if (!popup.classList.contains('popup_opened')) {
+function OpenPopup() {
+  if (popup.classList.contains('popup_opened') === false) {
     namePopup.value = nameProfile.textContent;
     jobPopup.value = jobProfile.textContent;
+    popup.classList.add('popup_opened');
   }
-  popup.classList.toggle('popup_opened');
+  else {
+    popup.classList.remove('popup_opened')
+  }
 }
-
-OpenPopupButton.addEventListener('click', togglePopup);
-ClosePopupButton.addEventListener('click', togglePopup);
 
 form.addEventListener('submit',
   function (event) {
       event.preventDefault();
       nameProfile.textContent = namePopup.value;
       jobProfile.textContent = jobPopup.value;
-      popup.classList.toggle('popup_opened');
+      OpenPopup()
   }
 );
+
+openPopupButton.addEventListener('click', OpenPopup);
+closePopupButton.addEventListener('click', OpenPopup);
+
+
+/*function togglePopup() {
+  if (!popup.classList.contains('popup_opened')) {
+    namePopup.value = nameProfile.textContent;
+    jobPopup.value = jobProfile.textContent;
+  }
+  popup.classList.toggle('popup_opened');
+}*/
+
+
